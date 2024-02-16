384b5b2884345cbd3932b4e3c7d0e320b2537ef2
Eliot-Shaw <eliot.shaw@proton.me>
2024-02-16 16:07:22 +0100

    bigfix

--- app/src/main/java/com/example/lamauvaiseruelle/AdManager.java
+++ app/src/main/java/com/example/lamauvaiseruelle/AdManager.java
@@ -21,59 +21,48 @@ public class AdManager {
 
     public ArrayList<AdModel> loadAdsFromJson(Context context) {
         ArrayList<AdModel> adModels = new ArrayList<>();
-        Log.e("claclowm", "debut");
 
-
-        // Ouvre le fichier JSON
         try {
             InputStream inputStream = context.getResources().openRawResource(R.raw.data);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             StringBuilder stringBuilder = new StringBuilder();
             String line;
-            Log.e("claclowm", "avant while");
+
             while ((line = bufferedReader.readLine()) != null) {
                 stringBuilder.append(line);
             }
             inputStream.close();
             String json = stringBuilder.toString();
-            Log.e("claclowm", "apreeswhile");
 
-            Log.e("JORSE", "0000");
+            // Convertir le JSON en un tableau d'objets JSON
+            JsonArray jsonArray = new Gson().fromJson(json, JsonArray.class);
 
-            Log.e("JORSE vistaprint", "JsonObject.class");
-            Log.e("JORSE vistaprint", (JsonObject.class).toString());
-            Log.e("JORSE vistaprint", "jose");
-            Log.e("JORSE vistaprint", (json).toString());
-
-            JsonObject jsonObject = new Gson().fromJson(json, JsonObject.class);
-
-
-
-            Log.e("JORSE", "1111");
-            JsonArray jsonArray = jsonObject.getAsJsonArray("data");
-
-            Log.e("claclowm", "avantfor");
+            // Parcourir chaque élément du tableau JSON
             for (JsonElement element : jsonArray) {
                 JsonObject adObject = element.getAsJsonObject();
-                String title = adObject.get("titre").getAsString();
-                String address = adObject.get("adresse").getAsString();
+
+                String title = adObject.get("title").getAsString();
+                String address = adObject.get("address").getAsString();
                 String nom_image = adObject.get("image").getAsString();
                 int image = context.getResources().getIdentifier(nom_image, "drawable", context.getPackageName());
-                if (image == 0){ //trouve pas l'image associée -> image générique
+                
+                if (image == 0) { 
+                    // Si l'image n'est pas trouvée, utiliser une image générique
                     image = context.getResources().getIdentifier("none", "drawable", context.getPackageName());
                     Log.w("AdManager", "Image pas trouvée");
-                    if (image == 0){ //image none pas trouvée -> ya un gros pb
+                    if (image == 0) { 
+                        // Si l'image générique n'est pas trouvée, il y a un problème grave
                         Log.e("AdManager", "Aucun lien avec les images");
                     }    
                 }
-                Log.e("claclowm", "la MAXXXX");
+                
+                // Ajouter l'objet AdModel à la liste
                 adModels.add(new AdModel(title, address, image));
             }
-            Log.e("claclowm", "apresfor");
         } catch (IOException e) {
             Log.e("AdManager", "Error reading JSON file", e);
         }
-        Log.e("claclowm", "fin");
+
         return adModels;
     }
 }
